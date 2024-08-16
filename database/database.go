51f9b7a50a83d847da5ccb4c264332dccbc77c35
Ali YILMAZ <aliylmzofficial@hotmail.com>
2024-08-16 11:48:59 +0300

    Structure created

---
+++ database/database.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os/user"
+	"strconv"
+
+	"github.com/aliyilmazdev/todo-list-restful-api/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	var err error
+	p := config.Config("DB_PORT")
+	port, err := strconv.ParseUint(p, 10, 32)
+
+	if err != nil {
+		log.Println("Error parsing port")
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=UTC", config.Config("DB_HOST"), config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"), port)
+
+	DB, err := gorm.Open(postgres.Open(dsn))
+
+	if err != nil {
+		log.Fatal("Error connecting to database")
+	}
+
+	fmt.Println("Database connected")
+
+	DB.AutoMigrate(&todo.Todo{})
+	DB.AutoMigrate(&user.User{})
+
+	fmt.Println("Database migrated")
+}
